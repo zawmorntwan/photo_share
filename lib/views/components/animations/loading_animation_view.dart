cae35cec89fd450f5a129fb32ca62e3e7db6e3be
Zaw Min Tun <75002871+zawmorntwan@users.noreply.github.com>
2024-01-04 17:37:18 +0630

    Create user posts view

--- lib/views/components/animations/loading_animation_view.dart
+++ lib/views/components/animations/loading_animation_view.dart
@@ -1,8 +1,8 @@
 import 'lottie_animation_view.dart';
 import 'models/lottie_animation.dart';
 
-class LoadinAnimationView extends LottieAnimationView {
-  const LoadinAnimationView({super.key})
+class LoadingAnimationView extends LottieAnimationView {
+  const LoadingAnimationView({super.key})
       : super(
           animation: LottieAnimation.loading,
         );
